a9c138c2451d4c8a4b8d37e36699e1f74d260d94
hayk ferayan <hayk715@gmail.com>
2018-08-12 16:53:34 -0700

    homework

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitter = {
+  consumer_key: 'KjjusKBO5pVhdkQ00qOvOoIBK',
+  consumer_secret: '7mRchpPd4CjIdP9oTMxCyzU4i9JFIZujoQ7yBjvMtuVWwmqg5z',
+  access_token_key: '959120381394042880-aoJDeCQxNkV1pUkCl2P4Gg82UpdDoEK',
+  access_token_secret: '0LWftGYANYE9HKObqGe9QEXfHEnBmfYtNTNbS9jvvKbTe',
+}
+
+exports.spotify = {
+	id: 'b6447a82f529414e863d5ada5a849e95',
+	secret: 'fd5bb4b78318466fab1d6ddccb20d2b0',
+}
